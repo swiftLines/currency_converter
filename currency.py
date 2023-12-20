0bbe0a0568dae370c0bf1ca178e2c913623d394c
Jeremy Underwood <underwood.j248@gmail.com>
2023-12-20 15:57:40 -0500

    add variables and initial definition

---
+++ currency.py
@@ -0,0 +1,10 @@
+import requests
+
+API_KEY = 'fca_live_pGiyeq6fkpcyHda2s24UOTUCM3mjg4bsn8u5dLYe'
+BASE_URL = f'https://api.freecurrencyapi.com/v1/latest?apikey={API_KEY}'
+
+CURRENCIES = ['USD', 'CAD', 'EUR', 'AUD', 'CNY']
+
+def convert_currency(base):
+  url = f'{BASE_URL}&base_currency={base}&currencies={}'
+
