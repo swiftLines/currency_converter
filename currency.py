7e03f5c52809a11552318636a68cdb3bfe62916e
Jeremy Underwood <underwood.j248@gmail.com>
2023-12-20 18:14:11 -0500

    add try and except block

--- currency.py
+++ currency.py
@@ -9,3 +9,13 @@ def convert_currency(base):
   currencies = ','.join(CURRENCIES)
   url = f'{BASE_URL}&base_currency={base}&currencies={currencies}'
 
+  try:
+    response = requests.get(url)
+    data = response.json()
+    print(data)
+    return data
+  except Exception as e:
+    print(e)
+    return None
+  
+convert_currency('CAD')
