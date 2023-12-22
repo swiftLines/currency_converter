5bff55a8883e8dbaa595dbbff2267ada887f991f
Jeremy Underwood <underwood.j248@gmail.com>
2023-12-21 20:29:23 -0500

    get input from user

--- currency.py
+++ currency.py
@@ -13,11 +13,20 @@ def convert_currency(base):
     response = requests.get(url)
     data = response.json()
     return data['data']
-  except Exception as e:
-    print(e)
+  except:
+    print('Currency invalid!')
     return None
-  
-data = convert_currency('CAD')
-del data['CAD']
-for ticker, value in data.items():
-  print(f'{ticker}: {value}')
+
+while True:
+  base = input('Enter the base currency (q for quit): ').upper()
+
+  if base == 'Q':
+    break
+
+  data = convert_currency(base)
+  if not data:
+    continue
+
+  del data[base]
+  for ticker, value in data.items():
+    print(f'{ticker}: {value}')
