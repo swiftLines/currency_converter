a9d6819a1091fe1dda43e8e4377a3a0fa9f5202d
Jeremy Underwood <underwood.j248@gmail.com>
2024-01-01 23:18:46 -0500

    secure API

--- currency.py
+++ currency.py
@@ -1,6 +1,5 @@
 import requests
 
-API_KEY = 'fca_live_pGiyeq6fkpcyHda2s24UOTUCM3mjg4bsn8u5dLYe'
 BASE_URL = f'https://api.freecurrencyapi.com/v1/latest?apikey={API_KEY}'
 
 CURRENCIES = ['USD', 'CAD', 'EUR', 'AUD', 'CNY']
